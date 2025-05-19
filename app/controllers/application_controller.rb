ee6574fe19ad69b042ea3e83ccdf3518c6517695
sk-mzs <sk_107@skm10noMacBook-Air.local>
2025-05-19 14:50:49 +0900

    Userテーブルにnameカラムを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -8,4 +9,12 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
+
+ def configure_permitted_parameters
+    added_attrs = [ :email, :password, :password_confirmation, :name ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
+
 end
