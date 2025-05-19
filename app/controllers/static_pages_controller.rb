59d64a8fc09151c776f664d54f70b0f9464c278b
sk-mzs <sk_107@skm10noMacBook-Air.local>
2025-05-19 22:56:29 +0900

    ランダム表示機能

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,8 @@
 class StaticPagesController < ApplicationController
-  def top; end
+  def top
+
+		random_post_id = Post.pluck(:id).sample
+		@random_post = Post.find_by(id: random_post_id)
+
+  end
 end
